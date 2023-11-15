d76ace850322ec78442f93c5df7d6ac8c11ebe6f
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-11-15 17:12:26 -0500

    Tutoria de RWD-terminada

---
+++ Front-End/Avance Sincrono/semana07/dia03-javascript-hangman/main.js
@@ -0,0 +1,16 @@
+const answer = "Javascript"
+
+const word = answer.toUpperCase()
+
+const generateAlphabets =[
+  'A','B','C','D','E','F','G','H',
+  'I','J','K','L','M','N','O','P',
+  'Q','R','S','T','U','V','W','X',
+  'Y','Z'
+]
+
+function renderMaskedWord(){
+  console.log(
+    word.split('')
+  )
+}

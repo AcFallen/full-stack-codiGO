bb350cc5de765d4c02a0824142077e62a4cf8dd8
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-11-16 21:37:27 -0500

    Llamado de apis(async/await)

--- Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
+++ Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
@@ -1,1 +1,29 @@
-console.log('Hola mundo')
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones'
+
+// function fetchCountries(){
+//   return fetch(url) // PROMISE
+//     .then(response => response.json())
+//     .catch(error => console.log(error))
+// }
+  // TODO: USAR la funcion usnado async/await
+
+const fetchCountries = async() => {
+    try {
+      const response = await fetch(url) // Promise
+
+      // IDENTIFICAMOS SI LA RESPUESTA FUE CORRECTA
+      if (!response.ok) {
+        throw new Error('ERROR!!!',response.status)
+      }
+
+      const json = await response.json()
+
+      return json
+
+    } catch (error) {
+      console.log(error)
+    }
+}
+
+fetchCountries()
+.then(data => console.log(data))

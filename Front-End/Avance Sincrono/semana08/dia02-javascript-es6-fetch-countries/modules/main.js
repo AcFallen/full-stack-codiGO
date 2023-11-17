e10d52ad6519c5877589527e9d1328b3314c54a3
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-11-16 22:59:41 -0500

    Segundo avance- Consumiendo API-Country

--- Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
+++ Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
@@ -1,11 +1,21 @@
-const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones'
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones,region'
 
+
+const searchInput = document.querySelector('.app__input')
+const filterSelect = document.querySelector('.app_filter')
 // function fetchCountries(){
 //   return fetch(url) // PROMISE
 //     .then(response => response.json())
 //     .catch(error => console.log(error))
 // }
-  // TODO: USAR la funcion usnado async/await
+
+searchInput.addEventListener('input',(event) => {
+  const value = event.target.value
+
+  console.log(value)
+})
+
+// TODO: USAR la funcion usnado async/await
 
 const fetchCountries = async() => {
     try {
@@ -25,5 +35,43 @@ const fetchCountries = async() => {
     }
 }
 
-fetchCountries()
-.then(data => console.log(data))
+const renderCountries = (countries) => {
+
+  const countryListElement = document.querySelector('.app__list')
+
+  let countryList = ''
+
+  countries.forEach(country => {
+    countryList += `
+    <div class="country">
+      <img class="country__flag" src="${country.flags.png}" alt="${country.flags.alt}">
+      <div class="country__wrapper">
+        <h2 class="country__title">${country.name.common}</h2>
+        <div class="country__description">
+          <strong>Population:</strong> ${country.population}
+        </div>
+        <div class="country__description">
+          <strong>Region:</strong> ${country.region}
+        </div>
+        <div class="country__description">
+          <strong>Capital:</strong> ${country.capital[0]}
+        </div>
+      </div>
+    </div>
+    `
+  })
+
+  countryListElement.innerHTML = countryList
+
+}
+
+document.addEventListener('DOMContentLoaded', async() =>{
+
+  // console.log('Mi DOOM ya cargo!!')
+
+  const data = await fetchCountries()
+
+  renderCountries(data)
+})
+
+

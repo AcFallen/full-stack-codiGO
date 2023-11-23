87cde02dc4681deecf124e9f472059b726230bff
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-11-23 04:22:09 -0500

    Avance clase retrasada min 1:50:14

--- Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
+++ Front-End/Avance Sincrono/semana08/dia02-javascript-es6-fetch-countries/modules/main.js
@@ -1,19 +1,56 @@
 const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones,region'
 
+let countryData = []
+
 
 const searchInput = document.querySelector('.app__input')
-const filterSelect = document.querySelector('.app_filter')
+const filterSelect = document.querySelector('.app__filter')
+const resultsDiv =document.querySelector('.app__results')
 // function fetchCountries(){
 //   return fetch(url) // PROMISE
 //     .then(response => response.json())
 //     .catch(error => console.log(error))
 // }
 
-searchInput.addEventListener('input',(event) => {
+searchInput.addEventListener('input', (event) => {
   const value = event.target.value
+  const loweredValue = value.toLowerCase()
 
-  console.log(value)
+  
+  const filteredCountries = countryData.filter(
+    country => {
+      const loweredName = country.name.common.toLowerCase()
+
+      // TODO: Adicionalmente necesitamos filtrar a los paises por su capital
+      const joinedCapitals = country.capital.join(',') // une todos los elementos de un arreglo en una cadena de texto
+      const loweredCapitals = joinedCapitals.toLowerCase()
+
+      return loweredName.includes(loweredValue) || loweredCapitals.includes(loweredValue) // true o false
+    }
+  )
+  renderCountries(filteredCountries)
+  renderResults(filteredCountries)
+
+  // console.log(value)
 })
+
+filterSelect.addEventListener('input', (event) => {
+
+  const value = event.target.value
+  const loweredValue = value.toLowerCase()
+
+  const filterCountriesByRegion =  countryData.filter(
+    country => {
+      const loweredRegion = country.region.toLowerCase()
+      
+      return loweredRegion.includes(loweredValue)
+    }
+  )
+
+  renderCountries(filterCountriesByRegion)
+
+})
+
 
 // TODO: USAR la funcion usnado async/await
 
@@ -33,6 +70,11 @@ const fetchCountries = async() => {
     } catch (error) {
       console.log(error)
     }
+}
+
+const renderResults = (countriesFiltered) => {
+  const total = countriesFiltered.length
+  resultsDiv.textContent = `${total} countries filtered`
 }
 
 const renderCountries = (countries) => {
@@ -70,6 +112,8 @@ document.addEventListener('DOMContentLoaded', async() =>{
   // console.log('Mi DOOM ya cargo!!')
 
   const data = await fetchCountries()
+
+  countryData = data
 
   renderCountries(data)
 })

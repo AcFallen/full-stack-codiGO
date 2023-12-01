0fbeabe6dddac7a2642036cdd8f8477df476d76d
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-11-30 22:01:19 -0500

    Semana10-Citas-Medicas-falta_actualziar

--- Front-End/Avance Sincrono/semana10/dia01-react-appointments-app/src/components/appointments/AppointmentsForm.jsx
+++ Front-End/Avance Sincrono/semana10/dia01-react-appointments-app/src/components/appointments/AppointmentsForm.jsx
@@ -1,26 +1,76 @@
-const AppointmentsForm = () => {
+import { useEffect, useState } from "react"
+
+const AppointmentsForm = ({ onSaveAppointment, appointment }) => {
+
+    const INITIAL_FORM_STATE = {
+        id:'',
+        petName:'',
+        petAge:'',
+        ownerName:'',
+        appointmentDate:'',
+        appointmentTime:'',
+        symptoms:''
+    }
+
+    const [form, setForm] = useState(INITIAL_FORM_STATE)
+
+    useEffect(() => {
+        console.log('Estoy en el formulario')
+        const hasAppointment = Object.keys(appointment).length > 0
+        if (hasAppointment) {
+            setForm(appointment)
+        }
+    },[appointment])
+
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+
+        setForm({ ...form, [name]: value })
+    }
+
+    const handleSaveAppointment = (event) => {
+        event.preventDefault()
+
+        const newAppointment ={
+            ...form,
+            id: crypto.randomUUID()
+        }
+
+        console.log('Guardando cita',newAppointment)
+
+        onSaveAppointment(newAppointment)
+
+        setForm(INITIAL_FORM_STATE)
+    }
+
     return(
         <section className="w-96 p-4">
             <h2 className="text-2xl text-center mb-4">Nuevo Paciente</h2>
 
-            <form className="flex flex-col gap-3">
-                <input type="text" name="petname" placeholder="Nombre de mi mascota"
-                className="border p-3 shadow-md rounded-md"/>
+            {/* <pre>
+                {JSON.stringify(form, null, 2)}
+            </pre> */}
+            
+
+            <form className="flex flex-col gap-3" onSubmit={handleSaveAppointment}>
+                <input type="text" name="petName" placeholder="Nombre de mi mascota"
+                className="border p-3 shadow-md rounded-md" onChange={handleChange} value={form.petName}/>
 
                 <input type="number" name="petAge" min="0" max="50" placeholder="Edad de la mascota"
-                className="border p-3 shadow-md rounded-md"/>
+                className="border p-3 shadow-md rounded-md" onChange={handleChange} value={form.petAge} />
 
-                <input type="number" name="ownerName" placeholder="Dueno de la mascota"
-                className="border p-3 shadow-md rounded-md"/>
+                <input type="text" name="ownerName" placeholder="Dueno de la mascota"
+                className="border p-3 shadow-md rounded-md" onChange={handleChange} value={form.ownerName}/>
 
-                <input type="date" name="appintmentDate" placeholder="Fecha de la cita"
-                className="border p-3 shadow-md rounded-md"/>
+                <input type="date" name="appointmentDate" placeholder="Fecha de la cita"
+                className="border p-3 shadow-md rounded-md" onChange={handleChange} value={form.appointmentDate} />
 
-                <input type="time" name="appintmentTime" placeholder="Hora de la cita"
-                className="border p-3 shadow-md rounded-md"/>
+                <input type="time" name="appointmentTime" placeholder="Hora de la cita"
+                className="border p-3 shadow-md rounded-md" onChange={handleChange} value={form.appointmentTime}/>
 
-                <textarea type="time" name="symtoms" placeholder="Sintomas"
-                className="border p-3 shadow-md rounded-md" rows={3}/>
+                <textarea type="time" name="symptoms" placeholder="Sintomas"
+                className="border p-3 shadow-md rounded-md" rows={3} onChange={handleChange} value={form.symptoms}/>
 
                 <input type="submit" className="bg-green-900 text-white p-2 rounded-lg " value="Guardar" />
             </form>
--- Front-End/Avance Sincrono/semana10/dia01-react-appointments-app/src/pages/Appointments.jsx
+++ Front-End/Avance Sincrono/semana10/dia01-react-appointments-app/src/pages/Appointments.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react"
 import AppointmentsForm from "../components/appointments/AppointmentsForm"
 import AppointmentsList from "../components/appointments/AppointmentsList"
 const Appointments = () => {
+
+    const [appointments, setAppointmens] = useState([])
+    const [appointmentSelected, setAppointmentSelected] = useState({})
+
+    const handleSaveAppointment = (form) => {
+        // Guardamos lo que nos entregue el AppointmentForm en el estado appointments
+        console.log(form)
+        setAppointmens([...appointments,form])
+    }
+
+    const handleRemove = (id) => {
+        const newAppointments = appointments.filter(
+            appointment => appointment.id != id
+        )
+
+        setAppointmens(newAppointments)
+    }
+
+    const handleEdit = (appointment) => {
+        console.log(appointment)
+        setAppointmentSelected(appointment)
+    }
+
     return(
         <>
-            <AppointmentsForm />
+            <AppointmentsForm 
+            onSaveAppointment = {handleSaveAppointment}
+            appointment= {appointmentSelected}
+            />
 
-            <AppointmentsList appointments={[1,2,3,4]} />
+            <AppointmentsList 
+                appointments={appointments} 
+                onRemove={handleRemove}
+                onEdit={handleEdit} />
         </>
     )
 }

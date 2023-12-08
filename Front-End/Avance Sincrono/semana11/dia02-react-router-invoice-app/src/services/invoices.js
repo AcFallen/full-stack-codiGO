8d05378f4e526ca4c67c184d094d61d8c406de41
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2023-12-07 23:21:25 -0500

    dia02-invoice-app-enviar-detalles

--- Front-End/Avance Sincrono/semana11/dia02-react-router-invoice-app/src/services/invoices.js
+++ Front-End/Avance Sincrono/semana11/dia02-react-router-invoice-app/src/services/invoices.js
@@ -1,7 +1,17 @@
-const API_URL = 'https://65728909d61ba6fcc0152a25.mockapi.io/api/v1/invoices/'
+const API_URL = 'https://65728909d61ba6fcc0152a25.mockapi.io/api/v1'
 
 export const fetchInvoices = async () => {
-    const url = `${API_URL}`
+    const url = `${API_URL}/invoices`
+
+    const response = await fetch(url)
+
+    const data = response.json()
+
+    return data
+  } 
+
+  export const getInvoice = async (id) => {
+    const url = `${API_URL}/invoices/${id}`
 
     const response = await fetch(url)
 
